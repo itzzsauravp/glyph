975452780e44e50db561dc0fba4da507565ac5a1
itzzsauravp <social.saurav2003@gmail.com>
2026-04-01 19:32:08 +0545

    chore: Update project configuration and settings

--- src/config/glyph.config.ts
+++ src/config/glyph.config.ts
@@ -6,10 +6,11 @@ export default class GlyphConfig {
 
         const config = vscode.workspace.getConfiguration('glyph');
         const model = config.get<string>('modelName', '');
+        const embeddingModel = config.get<string>('embeddingModelName', '');
         const endpoint = config.get<string>('base_url', 'http://localhost:11434');
         const autoSave = config.get<boolean>('autoSave', false);
 
-        return { model, endpoint, autoSave };
+        return { model, endpoint, autoSave, embeddingModel };
 
     }
 
@@ -18,6 +19,14 @@ export default class GlyphConfig {
         const config = vscode.workspace.getConfiguration('glyph');
         await config.update('modelName', newModel, vscode.ConfigurationTarget.Global);
         vscode.window.showInformationMessage(`Model updated to ${newModel}`);
+
+    }
+
+    public updateEmbeddingModel = async (newEmbeddingModel: string) => {
+
+        const config = vscode.workspace.getConfiguration('glyph');
+        await config.update('embeddingModelName', newEmbeddingModel, vscode.ConfigurationTarget.Global);
+        vscode.window.showInformationMessage(`Embedding Model updated to ${newEmbeddingModel}`);
 
     }
 
