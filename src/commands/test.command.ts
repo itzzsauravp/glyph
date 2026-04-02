9052b0969e37774892f0d5e432e78db507417fc6
itzzsauravp <social.saurav2003@gmail.com>
2026-04-02 15:29:38 +0000

    chore: resets the test command to be plain

--- src/commands/test.command.ts
+++ src/commands/test.command.ts
@@ -1,13 +1,8 @@
 import * as vscode from 'vscode';
-import type LLMService from '../services/llm.service';
-import type RepositoryIndexerService from '../services/repo-indexer.service';
 import BaseCommand from './base.command';
 
 export default class TestCommand extends BaseCommand {
-    constructor(
-        private readonly llmService: LLMService,
-        private readonly repoIndexer: RepositoryIndexerService,
-    ) {
+    constructor() {
         super();
     }
 
@@ -17,20 +12,5 @@ export default class TestCommand extends BaseCommand {
         vscode.window.showInformationMessage(
             'Hey there!!!\nThis is a Starter test command for glyph',
         );
-
-        const val = await this.llmService.identifyRequiredFiles(
-            'Just list all the files that you found.',
-            this.repoIndexer.parseDirectoryStructure(),
-        );
-
-        console.log('The val is:', val);
-
-        if (val && val.length > 0) {
-            vscode.window.showInformationMessage(
-                `Identified ${val.length} files: ${val.join(', ')}`,
-            );
-        } else {
-            vscode.window.showWarningMessage('No files were identified or the check failed.');
-        }
     };
 }
